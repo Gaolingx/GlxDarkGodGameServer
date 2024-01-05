9fffffd20dfe927c001ec619c5516b500fde2fe9
Gaolingx <947770192@qq.com>
2024-01-06 00:25:47 +0800

    查询玩家数据

--- DarkGodGameServer/DarkGod/Server/Server/04DB/DBMgr.cs
+++ DarkGodGameServer/DarkGod/Server/Server/04DB/DBMgr.cs
@@ -1,6 +1,7 @@
 ﻿//功能：数据库管理类
 
 
+using System;
 using MySql.Data.MySqlClient;
 using PEProtocol;
 
@@ -29,11 +30,73 @@ public class DBMgr
 
 
     //数据库账号查询
+    //根据账号和密码来查找数据库有没有对应的玩家数据
     public PlayerData? QueryPlayerData(string acct,string pass)
     {
+        bool isNew = true;
         PlayerData? playerData = null;
+        MySqlDataReader? reader = null;
 
-        //TODO
+        try
+        {
+            MySqlCommand cmd = new MySqlCommand("select * from account where acct = @acct", DBconn);
+            cmd.Parameters.AddWithValue("acct", acct);
+            reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                isNew = false;
+                string _pass = reader.GetString("pass");
+                //如果数据库中找到数据，则判断当前账号对应的密码和客户端传入的密码是否一致
+                if (_pass.Equals(pass))
+                {
+                    //如果相等则密码正确，返回玩家数据
+                    playerData = new PlayerData
+                    {
+                        id = reader.GetInt32("id"),
+                        name = reader.GetString("name"),
+                        lv = reader.GetInt32("level"),
+                        exp = reader.GetInt32("exp"),
+                        power = reader.GetInt32("power"),
+                        coin = reader.GetInt32("coin"),
+                        diamond = reader.GetInt32("diamond"),
+                        //TOADD
+                    };
+                }
+
+            }
+        }
+        catch (Exception e)
+        {
+            PECommon.Log("Query PlayerData By Acct&Pass Error:" + e, PELogType.Error);
+        }
+
+        //执行reader = cmd.ExecuteReader();时，如果数据库不存在玩家account，我们就需要默认为玩家创建一个新的账号
+        finally
+        {
+            if (isNew == true)
+            {
+                //不存在账号数据，创建新的默认账号数据，并返回
+                playerData = new PlayerData
+                {
+                    id = -1,
+                    name = "",
+                    lv = 1,
+                    exp = 0,
+                    power = 150,
+                    coin = 5000,
+                    diamond = 500,
+                    //TOADD
+                };
+                //获取主键id
+                playerData.id = InsertNewAcctData(acct, pass, playerData);
+            }
+        }
         return playerData;
+    }
+
+    //将新创建的账号数据插入到数据库中
+    private int InsertNewAcctData(string acct, string pass, PlayerData pd)
+    {
+
     }
 }

f0e5ea1621df1ea0d45ebe07fd665deccda6b982
Gaolingx <947770192@qq.com>
2024-01-15 11:23:11 +0800

    20240115_主城UI显示逻辑

--- DarkGodGameServer/DarkGod/Server/PEProtocol/PECommon.cs
+++ DarkGodGameServer/DarkGod/Server/PEProtocol/PECommon.cs
@@ -1,5 +1,6 @@
 ﻿//功能：客户端服务端共用工具类
 using PENet;
+using PEProtocol;
 
 public enum PELogType
 {
@@ -17,5 +18,21 @@ public class PECommon
     {
         LogLevel lv = (LogLevel)tp;
         PETool.LogMsg(msg, lv);
+    }
+
+    //战斗力计算
+    public static int GetFightByProps(PlayerData pd)
+    {
+        return pd.lv * 100 + pd.ad + pd.ap + pd.addef + pd.apdef;
+    }
+
+    public static int GetPowerLimit(int lv)
+    {
+        return ((lv - 1) / 10) * 150 + 150;
+    }
+
+    public static int GetExpUpValByLv(int lv)
+    {
+        return 100 * lv * lv;
     }
 }

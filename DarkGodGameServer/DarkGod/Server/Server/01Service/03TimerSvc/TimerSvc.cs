501743c60efb0bfd6e1b75ba8a90fc09c3a5311a
Gaolingx <947770192@qq.com>
2024-03-14 14:52:32 +0800

    20240314_使用独立线程检测定时

--- DarkGodGameServer/DarkGod/Server/Server/01Service/03TimerSvc/TimerSvc.cs
+++ DarkGodGameServer/DarkGod/Server/Server/01Service/03TimerSvc/TimerSvc.cs
@@ -3,6 +3,17 @@
 
 public class TimerSvc
 {
+    class TaskPack
+    {
+        public int tid;
+        public Action<int> cb;
+        public TaskPack(int tid, Action<int> cb)
+        {
+            this.tid = tid;
+            this.cb = cb;
+        }
+    }
+
     private static TimerSvc instance = null;
     public static TimerSvc Instance
     {
@@ -17,22 +28,50 @@ public class TimerSvc
     }
 
     PETimer pt = null;
+    Queue<TaskPack> tpQue = new Queue<TaskPack>();
+    private static readonly string tpQueLock = "tpQueLock";
 
     public void Init()
     {
-        pt = new PETimer();
+        pt = new PETimer(100); //每隔100ms进行一次定时任务检测
+        tpQue.Clear(); //清空Quene
 
         //设置定时器日志输出
         pt.SetLog((string info) =>
         {
             PECommon.Log(info);
         });
+
+        pt.SetHandle((Action<int> cb, int tid) =>
+        {
+            if (cb != null)
+            {
+                lock (tpQueLock)
+                {
+                    tpQue.Enqueue(new TaskPack(tid, cb)); //把满足目标的定时任务放入队列中
+                }
+            }
+        });
+
         PECommon.Log("TimerSvc Init Done.");
     }
 
-    public void Update()
+    public void Update() //使用独立的线程驱动定时器，优化性能
     {
-        pt.Update();
+        //检测到tpQue有满足条件的定时任务，立即执行
+        while (tpQue.Count > 0)
+        {
+            TaskPack tp = null;
+            lock (tpQueLock)
+            {
+                tp = tpQue.Dequeue();
+            }
+
+            if (tp == null)
+            {
+                tp.cb(tp.tid);
+            }
+        }
     }
 
     public int AddTimeTask(Action<int> callback, double delay, PETimeUnit timeUnit = PETimeUnit.Millisecond, int count = 1)
